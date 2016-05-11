f4b73c06235232e5d1ce375c129c2e630f604731
Christian Nwamba <nwambachristian@gmail.com>
2016-05-11 22:16:08 +0100

    Upstream Initial commit

---
+++ 01-User-Profile/app.js
@@ -0,0 +1,43 @@
+var app = angular.module('app', ['auth0', 'angular-storage']);
+
+app.config(function myAppConfig (authProvider) {
+  //TODO: Replace with dynmaic credentials and update using seperate file
+  authProvider.init({
+    domain: 'chris92.auth0.com',
+    clientID: 'G7IwfyxcX3O5dHU2ikjdOXDpa7HAtjyr'
+  });
+
+    //Called when login is successful
+  authProvider.on('loginSuccess', function($rootScope, profile) {
+    console.log("User profile from config event: ", profile);
+    $rootScope.profile = profile;
+  });
+
+  //Called when login fails
+  authProvider.on('loginFailure', function() {
+    alert("Error");
+  });
+
+  //Called when user is authenticated (Authentication flag is true)
+  authProvider.on('authenticated', function() {
+    console.log("Authenticated");
+  });
+});
+
+app.controller('LoginCtrl', function ($scope, auth, store) {
+  // Add auth to $scope object so we can bind to view
+  $scope.auth = auth;
+
+  $scope.signin = function (){
+    auth.signin({}, //The first argument is scope of data you need to return
+      function(profile, idToken){
+        // Store user profile
+        store.set('profile', profile);
+        $scope.token = idToken;
+        $scope.controllerProfile = profile;
+      },
+      function(err) {
+        $scope.err = err;
+      });
+  }
+});
