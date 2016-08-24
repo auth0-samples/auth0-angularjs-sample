55d2edc6b2cf339a3f616d8f605ad005fc78cfbf
Roberto Romero <sildurin@gmail.com>
2016-08-24 15:40:30 +0200

    Update 02-Custom-Login so it uses the new wrapper
    Update 02-Custom-Login so it uses the new dynamic wrapper around auth0.js

---
+++ 02-Custom-Login/app.js
@@ -0,0 +1,47 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('app', ['auth0.auth0', 'angular-jwt', 'ngRoute'])
+    .config(config);
+
+    config.$inject = ['$routeProvider', '$httpProvider', 'angularAuth0Provider', 'jwtOptionsProvider', 'jwtInterceptorProvider'];
+
+    function config($routeProvider, $httpProvider, angularAuth0Provider, jwtOptionsProvider, jwtInterceptorProvider) {
+
+      // Initialization for the angular-auth0 library
+      angularAuth0Provider.init({
+        clientID: AUTH0_CLIENT_ID,
+        domain: AUTH0_DOMAIN
+      });
+
+      // Configuration for angular-jwt
+      jwtOptionsProvider.config({
+        tokenGetter: function() {
+          return localStorage.getItem('id_token');
+        },
+        whiteListedDomains: ['localhost'],
+        unauthenticatedRedirectPath: '/login'
+      });
+
+      // Add the jwtInterceptor to the array of HTTP interceptors
+      // so that JWTs are attached as Authorization headers
+      $httpProvider.interceptors.push('jwtInterceptor');
+
+      $routeProvider
+        .when('/', {
+          controller: 'homeController',
+          templateUrl: 'components/home/home.html'
+        })
+        .when('/login', {
+          controller: 'loginController',
+          templateUrl: 'components/login/login.html'
+        })
+        .when('/ping', {
+          controller: 'pingController',
+          templateUrl: 'components/ping/ping.html'
+        });
+    }
+
+})();
---
+++ 02-Custom-Login/components/login/login.controller.js
@@ -0,0 +1,54 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('loginController', loginController);
+
+    loginController.$inject = ['$scope', 'authService'];
+
+    function loginController($scope, authService) {
+
+      // Put the authService on $scope to access
+      // the login method in the view
+      $scope.authService = authService;
+
+      $scope.login = function () {
+        // Show loading indicator
+        $scope.message = 'loading...';
+        $scope.loading = true;
+        authService.login($scope.user, $scope.pass, function(err) {
+          if(err) {
+            $scope.message = "something went wrong: " + err.message;
+            $scope.loading   = false;
+          }
+        });
+      };
+
+      $scope.signup = function () {
+        // Show loading indicator
+        $scope.message = 'loading...';
+        $scope.loading = true;
+        authService.signup($scope.user, $scope.pass, function(err) {
+          if(err) {
+            $scope.message = "something went wrong: " + err.message;
+            $scope.loading = false;
+          }
+        });
+      };
+
+      $scope.googleLogin = function () {
+        $scope.message = 'loading...';
+        $scope.loading = true;
+
+        authService.googleLogin(function(err) {
+          if(err) {
+            $scope.message = "something went wrong: " + err.message;
+            $scope.loading = false;
+          }
+        });
+      };
+    }
+
+})();
