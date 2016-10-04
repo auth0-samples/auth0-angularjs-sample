23e102bf0d7629a98d113fb6d03520a6010986ca
Valerii Radchenko <valerii.radchenko@akvelon.com>
2016-10-04 12:38:47 +0300

    07-Authorization

---
+++ 07-Authorization/components/auth/auth.service.js
@@ -0,0 +1,92 @@
+(function () {
+
+  'use strict';
+
+  angular
+    .module('app')
+    .service('authService', authService);
+
+  authService.$inject = ['$rootScope', 'lock', 'authManager', 'jwtHelper', '$q'];
+
+  function authService($rootScope, lock, authManager, jwtHelper, $q) {
+
+    var userProfile = JSON.parse(localStorage.getItem('profile')) || null;
+    var deferredProfile = $q.defer();
+
+    if (userProfile) {
+      deferredProfile.resolve(userProfile);
+    }
+
+    function login() {
+      lock.show();
+    }
+
+    // Logging out just requires removing the user's
+    // id_token and profile
+    function logout() {
+      deferredProfile = $q.defer();
+      localStorage.removeItem('id_token');
+      localStorage.removeItem('profile');
+      authManager.unauthenticate();
+      userProfile = null;
+    }
+
+    // Set up the logic for when a user authenticates
+    // This method is called from app.run.js
+    function registerAuthenticationListener() {
+      lock.on('authenticated', function (authResult) {
+        localStorage.setItem('id_token', authResult.idToken);
+        authManager.authenticate();
+
+        lock.getProfile(authResult.idToken, function (error, profile) {
+          if (error) {
+            return console.log(error);
+          }
+
+          localStorage.setItem('profile', JSON.stringify(profile));
+          userProfile = profile;
+          deferredProfile.resolve(profile);
+        });
+
+      });
+    }
+
+    function getProfileDeferred() {
+      return deferredProfile.promise;
+    }
+
+    function checkAuthOnRefresh() {
+      var token = localStorage.getItem('id_token');
+      if (token) {
+        if (!jwtHelper.isTokenExpired(token)) {
+          if (!$rootScope.isAuthenticated) {
+            authManager.authenticate();
+          }
+        }
+      }
+    }
+
+    function isAdmin() {
+      return userProfile && userProfile.app_metadata
+        && userProfile.app_metadata.roles
+        && userProfile.app_metadata.roles.indexOf('admin') > -1;
+    }
+
+    $rootScope.$on('$stateChangeStart', function(event, nextRoute) {
+      if (nextRoute.controller === 'AdminController') {
+        if (!isAdmin()) {
+          alert('You are not allowed to see the Admin content');
+          return event.preventDefault();
+        }
+      }
+    });
+
+    return {
+      login: login,
+      logout: logout,
+      registerAuthenticationListener: registerAuthenticationListener,
+      checkAuthOnRefresh: checkAuthOnRefresh,
+      getProfileDeferred: getProfileDeferred
+    }
+  }
+})();
