e6876ed00932a7fcbda1b21275b3b618740ecf20
Valerii Radchenko <valerii.radchenko@akvelon.com>
2016-10-06 16:41:15 +0300

    12-Passwordless-Lock

---
+++ 12-Passwordless-Lock/components/auth/auth.service.js
@@ -0,0 +1,68 @@
+(function () {
+
+  'use strict';
+
+  angular
+    .module('app')
+    .service('authService', authService);
+
+  authService.$inject = ['$rootScope', 'lockPasswordless', 'authManager', 'jwtHelper', '$q', '$state'];
+
+  function authService($rootScope, lockPasswordless, authManager, jwtHelper, $q, $state) {
+
+    var userProfile = JSON.parse(localStorage.getItem('profile')) || null;
+    var deferredProfile = $q.defer();
+
+    if (userProfile) {
+      deferredProfile.resolve(userProfile);
+    }
+
+    function login() {
+      lockPasswordless.emailcode(function(error, profile, id_token) {
+        if (error) {
+          alert("Error: " + error);
+          return;
+        }
+        localStorage.setItem('id_token', id_token);
+        authManager.authenticate();
+        localStorage.setItem('profile', JSON.stringify(profile));
+
+        deferredProfile.resolve(profile);
+        $state.go('home');
+        lockPasswordless.close();
+      });
+    }
+
+    // Logging out just requires removing the user's
+    // id_token and profile
+    function logout() {
+      deferredProfile = $q.defer();
+      localStorage.removeItem('id_token');
+      localStorage.removeItem('profile');
+      authManager.unauthenticate();
+      userProfile = null;
+    }
+
+    function getProfileDeferred() {
+      return deferredProfile.promise;
+    }
+
+    function checkAuthOnRefresh() {
+      var token = localStorage.getItem('id_token');
+      if (token) {
+        if (!jwtHelper.isTokenExpired(token)) {
+          if (!$rootScope.isAuthenticated) {
+            authManager.authenticate();
+          }
+        }
+      }
+    }
+
+    return {
+      login: login,
+      logout: logout,
+      checkAuthOnRefresh: checkAuthOnRefresh,
+      getProfileDeferred: getProfileDeferred
+    }
+  }
+})();
