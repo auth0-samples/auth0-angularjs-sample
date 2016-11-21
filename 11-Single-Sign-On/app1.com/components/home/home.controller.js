dc162722d5a56a25e3be4bdf420f62ce171be630
Ryan Chenkie <chenkie@users.noreply.github.com>
2016-11-21 12:01:46 -0500

    update logout

--- 11-Single-Sign-On/app1.com/components/home/home.controller.js
+++ 11-Single-Sign-On/app1.com/components/home/home.controller.js
@@ -18,7 +18,10 @@
     });
 
     vm.logoutFromAuth0 = function() {
-      angularAuth0.logout({returnTo: 'http://localhost:3000/'});
+      angularAuth0.logout({
+        returnTo: 'http://localhost:3000/',
+        client_id: AUTH0_CLIENT_ID
+      });
       authService.logout();
     }
 
